b19d898d406fc56bb6bfa498ceb7d100931c7b9b
Eric Kim <hyeokjungkim@gmail.com>
2018-05-24 19:46:00 -0400

    added more exports

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,10 +2,11 @@ class AuthenticationController < ApplicationController
  skip_before_action :authorize_request
 
  def authenticate
+   @user = User.find_by(username: params[:authentication][:username])
    command = AuthenticateUser.call(params[:authentication][:username], params[:authentication][:password])
 
    if command.success?
-     render json: { auth_token: command.result }
+     render json: { token: command.result, username: @user.username, user_id: @user.id }
    else
      render json: { error: command.errors }, status: :unauthorized
    end
