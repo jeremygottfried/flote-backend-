794be6af4cf3e036088014d32de8ce7eb73e1fb1
github username <jeremyagottfried@gmail.com>
2018-05-28 17:57:25 -0400

    refactored action cable websockets to fit with frontend

--- app/channels/note_channel.rb
+++ app/channels/note_channel.rb
@@ -1,7 +1,6 @@
 class NoteChannel < ApplicationCable::Channel
 
   def subscribed
-
     @user = User.find_by(username: params[:username])
     @channel = Channel.find_or_create_by(id: params[:room])
     @user.channels << @channel
@@ -14,17 +13,20 @@ class NoteChannel < ApplicationCable::Channel
   end
 
   def receive(data)
-    byebug
-    @user = User.find_by(username: data['username'])
+
+    @user = User.find_by(username: params['username'])
     @channel = Channel.find_or_create_by(id: params[:room])
-    @note_data = {user: @user, message: data["note"], channel: @channel}
+    @note_data = {user: @user, body: data["note"], channel: @channel}
+
     @note = Note.new(@note_data)
     if @note.valid?
       @note.save
       data["note_type"] = "note"
-      ActionCable.server.broadcast "note_#{params[:room_id]}", data
+      data["id"] = @note.id
+      data["body"] = @note.body
+      ActionCable.server.broadcast "note_#{params[:room]}", data
     else
-      ActionCable.server.broadcast "note_#{params[:room_id]}", {message_type: "note_error", errors: @note.errors, notice: "errors", status: :unprocessable_entity}
+      ActionCable.server.broadcast "note_#{params[:room]}", {message_type: "note_error", errors: @note.errors, notice: "errors", status: :unprocessable_entity}
     end
   end
 end
